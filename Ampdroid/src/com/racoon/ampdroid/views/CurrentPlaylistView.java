0ee6a855715cdd3f41bb54a3e60f6e49a087ee81
Daniel Schruhl <danielschruhl@gmail.com>
2014-07-29 12:29:59 +0200

    empty the currently playing playlist

--- Ampdroid/src/com/racoon/ampdroid/views/CurrentPlaylistView.java
+++ Ampdroid/src/com/racoon/ampdroid/views/CurrentPlaylistView.java
@@ -13,12 +13,16 @@ import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.racoon.ampache.Song;
 import com.racoon.ampdroid.Controller;
@@ -39,6 +43,7 @@ public class CurrentPlaylistView extends Fragment {
 	private TextView duration;
 	private TextView currentDuration;
 	private TextView songTitle;
+	private ListView playlist;
 
 	/**
 	 * 
@@ -57,9 +62,9 @@ public class CurrentPlaylistView extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		controller = Controller.getInstance();
 		ViewGroup root = (ViewGroup) inflater.inflate(R.layout.current_playlist, null);
-		ListView listview = (ListView) root.findViewById(R.id.playNow_listview);
+		playlist = (ListView) root.findViewById(R.id.playNow_listview);
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-			listview.setFastScrollAlwaysVisible(true);
+			playlist.setFastScrollAlwaysVisible(true);
 		}
 		seekBar = (SeekBar) root.findViewById(R.id.playNow_seekbar);
 		songTitle = (TextView) root.findViewById(R.id.playNow_song);
@@ -72,8 +77,8 @@ public class CurrentPlaylistView extends Fragment {
 		Log.d("songs:", list.toString());
 		StableArrayAdapter adapter = new StableArrayAdapter(getActivity().getApplicationContext(),
 				R.layout.content_list_item, list);
-		listview.setAdapter(adapter);
-		listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+		playlist.setAdapter(adapter);
+		playlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
 				controller.setPlayingNow(controller.getPlayNow().get(position));
@@ -100,7 +105,30 @@ public class CurrentPlaylistView extends Fragment {
 			}
 
 		});
+		setHasOptionsMenu(true);
 		return root;
 	}
 
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		inflater.inflate(R.menu.currently_playing_menu, menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.edit_discard:
+			controller.getPlayNow().clear();
+			ArrayList<String> list = new ArrayList<String>();
+			for (Song s : controller.getPlayNow()) {
+				list.add(s.toString());
+			}
+			StableArrayAdapter adapter = new StableArrayAdapter(getActivity().getApplicationContext(),
+					R.layout.content_list_item, list);
+			playlist.setAdapter(adapter);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
 }
